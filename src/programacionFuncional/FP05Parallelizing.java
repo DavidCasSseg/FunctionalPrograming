ac2872de31dc4dc6820fa98727a6a9ca1509b85e
DAVID <davcas@hotmail.com>
2024-03-08 14:54:48 -0600

    Create FP05Parallelizing.java
    using parallel to reduce the compilation time

---
+++ src/programacionFuncional/FP05Parallelizing.java
@@ -0,0 +1,16 @@
+package programacionFuncional;
+
+import java.util.stream.LongStream;
+
+public class FP05Parallelizing {
+
+    public static void main(String[] args) {
+
+        long time = System.currentTimeMillis();
+        //0, 10000000000
+        //System.out.println(LongStream.range(0,1000000000).sum());//499999999500000000 // time 1510
+        System.out.println(LongStream.range(0,1000000000).parallel().sum());//t
+        System.out.println(System.currentTimeMillis() - time);
+    }
+
+}
