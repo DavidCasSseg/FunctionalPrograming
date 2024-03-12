f138686fc94ffdf695912b9ea7b706f5941a1f4e
DAVID <davcas@hotmail.com>
2024-03-12 17:40:33 -0600

    Update ThreadBasicsRunner.java
    uses thread to run parallel

--- src/Multithreading/ThreadBasicsRunner.java
+++ src/Multithreading/ThreadBasicsRunner.java
@@ -1,17 +1,42 @@
 package Multithreading;
 
-public class ThreadBasicsRunner {
-    public static void main(String[] args) {
-        //Task1
+//extends Thread
+//implements Runnable
+class Task1 extends  Thread{
+
+    public void run(){//SIGNATURE
+        System.out.println("\nTask1 Started");
         for(int i = 101; i <= 199; i++){
             System.out.print(i + " ");
         }
         System.out.print("\n Task1 Done\n");
-        //Task2
+    }
+}
+
+class Task2 implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("\nTask2 Started");
         for(int i = 201; i <= 299; i++){
             System.out.print(i + " ");
         }
         System.out.print("\n Task2 Done\n");
+    }
+}
+public class ThreadBasicsRunner {
+    public static void main(String[] args) {
+        //Task1
+        System.out.print("\nTask1 Kicked off");
+        Task1 task1 = new Task1();
+        task1.start();//task1.run();
+
+        System.out.print("\nTask2 Kicked off");
+        //Task2
+        Task2 task2 = new Task2();
+        Thread threadTask2 = new Thread(task2);
+        threadTask2.start();
+
+        System.out.println("\n Task3 Kicked off");
         //Task3
         for(int i = 301; i <= 399; i++){
             System.out.print(i + " ");
