a17e5e5286034bb1923754672e0f1ec7acbd1708
DAVID <davcas@hotmail.com>
2024-03-12 15:50:01 -0600

    Update ThreadBasicsRunner.java
    little update

--- src/Multithreading/ThreadBasicsRunner.java
+++ src/Multithreading/ThreadBasicsRunner.java
@@ -6,17 +6,17 @@ public class ThreadBasicsRunner {
         for(int i = 101; i <= 199; i++){
             System.out.print(i + " ");
         }
-        System.out.print("\n Task1 Done");
+        System.out.print("\n Task1 Done\n");
         //Task2
         for(int i = 201; i <= 299; i++){
             System.out.print(i + " ");
         }
-        System.out.print("\n Task2 Done");
+        System.out.print("\n Task2 Done\n");
         //Task3
         for(int i = 301; i <= 399; i++){
             System.out.print(i + " ");
         }
-        System.out.print("\n Task3 Done");
+        System.out.print("\n Task3 Done\n");
 
         System.out.print("\n Main Done");
     }
