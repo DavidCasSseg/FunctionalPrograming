ee1af73933359361cdaa14ec9055f99bf85ecb19
DAVID <davcas@hotmail.com>
2024-04-01 17:55:57 -0600

    01-04-2024

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(test());
+    }
+
+    public static int test() {
+        try {
+            return 0;
+        } finally {
+            return 1;
+        }
+    }
+}
--- src/files/DirectoryScanRunner.java
+++ src/files/DirectoryScanRunner.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
 public class DirectoryScanRunner {
@@ -13,10 +15,23 @@ public class DirectoryScanRunner {
 		Path currentDirectory = Paths.get(".");
 //		Files.list(currentDirectory).forEach(System.out::println);
 
-		Predicate<? super Path> predicate = path -> String.valueOf(path).contains(".java");
+		Predicate<? super Path> predicate
+				= path -> String.valueOf(path).contains(".java");
 
-		Files.walk(currentDirectory, 2)
+		Files.walk(currentDirectory, 4)
 				.filter(predicate)
+				.forEach(System.out::println);
+
+		BiPredicate<Path, BasicFileAttributes> matcher
+				= ((path, attributes) -> String.valueOf(path).contains(".java"));
+
+		BiPredicate<Path, BasicFileAttributes> directoryMatcher
+				= (path, attributes) -> attributes.isDirectory();
+
+		Files.find(currentDirectory, 4, directoryMatcher)
+				.forEach(System.out::println);
+
+		Files.find(currentDirectory, 4, matcher)
 				.forEach(System.out::println);
 	}
 
