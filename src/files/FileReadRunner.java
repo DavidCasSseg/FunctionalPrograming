f5a2c4c0de47cba3705c14de4625d9a2033b915d
DAVID <davcas@hotmail.com>
2024-04-01 18:00:18 -0600

    start reading lines of a file

---
+++ src/files/FileReadRunner.java
@@ -0,0 +1,15 @@
+package files;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileReadRunner {
+    public static void main(String[] args) {
+
+        Path pathFileToRead = Paths.get("./resources/data.text");
+
+       // List<String> lines = Files.readAllLines(pathFileToRead);
+    }
+}
