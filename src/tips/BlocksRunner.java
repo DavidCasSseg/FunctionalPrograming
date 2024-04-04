9068d614c5c20fce1bbf14c6a152c0dd5bcdeca4
DAVID <davcas@hotmail.com>
2024-04-04 17:59:52 -0600

    mas tips

---
+++ src/tips/BlocksRunner.java
@@ -0,0 +1,12 @@
+package tips;
+
+public class BlocksRunner {
+    public static void main(String[] args) {
+        if(3>2)
+            System.out.println("3>2");
+        {
+            System.out.println("2>3");
+        }
+
+    }
+}
--- src/tips/ImportsRunner.java
+++ src/tips/ImportsRunner.java
@@ -1,7 +1,17 @@
 package tips;
 
+import java.util.ArrayList;
+
+import static java.lang.System.out;
+import static java.util.Collections.*;
+
 public class ImportsRunner {
     public static void main(String[] args) {
+
+        out.println("Imports");
+        out.println("Static Import");
+
+        sort(new ArrayList<String>());
 
         String str = "";
     }
